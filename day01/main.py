adfddd291079dd07a1bd8e89f2a40d1b72b9740e
paytonshafer <paytonshafer@gmail.com>
2022-12-01 14:52:03 -0500

    Day 1 complete

---
+++ day01/main.py
@@ -0,0 +1,43 @@
+#Part 1
+sum = 0
+maxSum = 0
+
+with open("/Users/paytonshafer/Documents/GitHub/advent-of-code-2022/day1/input.txt","r") as input:
+    line = input.readline()
+    while line:
+        if (line == "\n"):
+            if sum > maxSum:
+                maxSum = sum
+            sum = 0
+        else:
+            sum = sum + int(line)
+
+        line = input.readline()
+
+print(maxSum)
+
+
+#Part 2
+sum = 0
+count = 0
+maxSums = [0,0,0]
+
+with open("/Users/paytonshafer/Documents/GitHub/advent-of-code-2022/day1/input.txt","r") as input:
+    line = input.readline()
+    while line:
+        if (line == "\n"):
+            if count == 0 or count == 1 or count ==2:
+                maxSums[count] = sum
+            elif min(maxSums) < sum:
+                maxSums[maxSums.index(min(maxSums))] = sum
+            count += 1
+            sum = 0
+        else:
+            sum = sum + int(line)
+
+        line = input.readline()
+
+result = 0
+for i in range(3):
+    result = result + maxSums[i]
+print(result)
